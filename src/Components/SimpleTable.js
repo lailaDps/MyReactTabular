13541f9c83febee0a0c2dc30f2fa8b447daf3713
Laila BEN-ABDELAZIZ <laila.ben-abdelaziz@dps.local>
2017-11-16 12:19:41 +0100

    Create a simple table finally!

---
+++ src/Components/SimpleTable.js
@@ -0,0 +1,89 @@
+import * as Table from 'reactabular-table';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class SimpleTable extends Component{         
+  constructor(props) {
+    super(props);
+    this.countries = {
+      fi: 'Finland',
+      dk: 'Denmark'
+    };
+   
+    this.state = {
+       rows : [
+                    {
+                      id: 100,
+                      name: 'John',
+                      tools: {
+                        hammer: true
+                      },
+                      country: 'fi'
+                    },
+                    {
+                      id: 101,
+                      name: 'Jack',
+                      tools: {
+                        hammer: false
+                      },
+                      country: 'dk'
+                    }
+      ],
+       
+       columns : [
+                  {
+                    property: 'name',
+                    header: {
+                      label: 'Name',
+                      transforms: [
+                        label => ({
+                          onClick: () => alert(`clicked ${label}`)
+                        })
+                      ]
+                    }
+                  },
+                  {
+                    property: 'tools',
+                    header: {
+                      label: 'Active',
+                      transforms: [
+                        label => ({
+                          onClick: () => alert(`clicked ${label}`)
+                        })
+                      ]
+                    },
+                    cell: {
+                      formatters: [
+                        tools => tools.hammer ? 'Hammertime' : 'nope'
+                      ]
+                    }
+                  },
+                  {
+                    property: 'country',
+                    header: {
+                      label: 'Country',
+                      transforms: [
+                        label => ({
+                          onClick: () => alert(`clicked ${label}`)
+                        })
+                      ]
+                    },
+                    cell: {
+                      formatters: [
+                        country => this.countries[country]
+                      ]
+                    }
+                  },
+      ]
+    };} 
+   
+render(){
+  
+  return( 
+  <Table.Provider className="pure-table pure-table-striped" columns={this.state.columns} ><Table.Header />
+  <Table.Body rows={this.state.rows} rowKey="id" /></Table.Provider>
+  )
+  }
+}
+export default SimpleTable;
